3ad4c45812cbb5a3af98de1008219b760d63647e
Piyush55dude <piyushagarwalvo@gmail.com>
2023-07-07 16:47:59 +0530

    Sliding Window Maximum

--- #5 - Queue/4-sliding-window-maximum.js
+++ #5 - Queue/4-sliding-window-maximum.js
@@ -1,7 +1,8 @@
 // Ques 4 : Sliding Window Maximum
 // You are given an array of integers nums, there is a sliding window of size k which is
 // moving from the very left of the array to the very right.You can only see the k numbers
-// in the window. Each time the sliding window moves right by one position.
+// in the window. Each time the sliding window moves right by one position. For each window,
+// take the maximum element and add them to a final result array.
 
 // Input: nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
 // Output: [3, 3, 5, 5, 6, 7]
